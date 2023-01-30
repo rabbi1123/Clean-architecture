a442e9d536c8d299a330f276aa0630c9bd17fa9d
rabbi1123 <mushfikur80@gmail.com>
2023-01-30 14:25:04 +0600

    FirstConfigure

--- Clean.Api/Program.cs
+++ Clean.Api/Program.cs
@@ -1,7 +1,10 @@
+using Clean.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
-
+IConfiguration Configuration;
+Configuration = builder.Configuration;
 // Add services to the container.
-
+builder.Services.AddInfrastructure(Configuration);
 builder.Services.AddControllers();
 
 var app = builder.Build();
---
+++ Clean.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Clean.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clean.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDBContext>(options => options.UseSqlServer(configuration.GetConnectionString("connection")));
+
+            return services;
+        }
+    }
+}
